539268ba175a467669a1d10690f4dfdd33ac5dee
Raj Pratap Singh Gurjar <rajpragur@gmail.com>
2025-10-10 01:03:22 +0530

    update chunksize to 1.6MB

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig(({ mode }) => ({
       port: 8080,
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
   plugins: [
     react(),
     mode === 'development' &&
